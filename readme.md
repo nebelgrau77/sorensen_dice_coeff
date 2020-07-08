0b14303c987d2486c350fe9dd77c04095dfe7d7c
nebelgrau <nebelgrau@gmx.com>
2020-07-08 13:09:37 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+### Sørensen–Dice coefficient calculator
+
+https://en.wikipedia.org/wiki/Sørensen–Dice_coefficient
+
+Calculates the Sørensen–Dice coefficient between two strings supplied as arguments: 
+
+_sdcoeff \<word_a\> \<word_b\>_
---
+++ src/main.rs
@@ -0,0 +1,86 @@
+extern crate clap;
+
+use clap::{AppSettings, App, Arg};
+
+fn main() {
+
+    let matches = App::new("Sorensen-Dice coefficient")
+                    .about("Calculates Sorensen-Dice coefficient between two words.")
+                    .setting(AppSettings::DisableVersion)
+                    .arg(
+                        Arg::with_name("word_A")
+                        .help("First word")
+                        .use_delimiter(false)
+                        .required(true),
+                    )
+                    .arg(
+                        Arg::with_name("word_B")
+                        .help("Second word")
+                        .use_delimiter(false)
+                        .required(true),
+                    )
+                    .get_matches();
+
+    let word_a = matches.value_of("word_A").unwrap();
+    let word_b = matches.value_of("word_B").unwrap();
+
+    let word_a = word_a.trim().to_ascii_lowercase();
+
+    let word_b = word_b.trim().to_ascii_lowercase();
+
+    let coeff: f32 = sd_coeff(&word_a, &word_b);
+    
+    println!("Sørensen-Dice coefficient is {:.03}", coeff);
+      
+}
+
+
+fn sd_coeff(word_a: &str, word_b: &str) -> f32 {
+    
+    // calculate Sørensen–Dice coefficient
+
+    let bigrams_a = get_bigrams(word_a);
+    let bigrams_b = get_bigrams(word_b);
+    
+    let common = compare_bigrams(&bigrams_a, &bigrams_b);
+    
+    let coeff: f32 = (2.0 * common as f32) / (bigrams_a.len() + bigrams_b.len()) as f32;
+    
+    return coeff;
+    
+}
+
+fn get_bigrams(word: &str) -> Vec<&str> {
+    
+    // split a word into bigrams
+
+    let mut bigrams: Vec<&str> = Vec::new();
+    
+    for i in 0..(word.len()-1) {
+        
+        let bigram = &word[i..i+2];
+        
+        bigrams.push(bigram);
+    }
+
+    return bigrams;
+    
+}
+
+fn compare_bigrams(bigrams_a: &[&str], bigrams_b: &[&str]) -> u8 {
+    
+    // count how many bigrams are common between two words
+
+    let mut common: u8 = 0;
+    
+    for item_a in bigrams_a.iter() {
+        for item_b in bigrams_b.iter() {
+            if item_a == item_b {
+                common += 1;
+            }
+        }
+    }
+
+    return common; 
+    
+}
